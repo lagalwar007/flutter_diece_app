aa88d62be2026132fdc87698f20bd98f10fa125e
karan-lagalwar <lagalwar007@gmail.com>
2021-10-12 18:07:26 +0530

    save changes: Diece app UI finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,69 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int rightDiceNumberEnd = 1;
+
+
+  void updateRandomDieceNum(){
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber =  Random().nextInt(6) + 1;
+    rightDiceNumberEnd =  Random().nextInt(6) + 1;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  updateRandomDieceNum();
+                });
+                print('Left button ===> $leftDiceNumber');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: (){
+                setState(() {
+                  updateRandomDieceNum();
+                });
+                print('Right button ====> $rightDiceNumber');
+              },
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumberEnd.png'),
+              onPressed: (){
+                setState(() {
+                  updateRandomDieceNum();
+                });
+                print('Right button ====> $rightDiceNumberEnd');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
